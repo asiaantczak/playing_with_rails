db415a7cece053353df402589f4bdeb1e267f1a9
Asia Antczak <aasiaantczak@gmail.com>
2018-06-05 10:52:47 +0100

    create users

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_124518) do
+ActiveRecord::Schema.define(version: 2018_06_05_082658) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
